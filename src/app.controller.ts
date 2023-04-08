71a10b9fc7ded8aa76d041eddc383fa80a4a175f
Gwonyeong <rnjsdud980@naver.com>
2023-04-08 09:54:55 +0900

    Add: 영화 키워드 추천 api docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -155,6 +155,14 @@ export class VideoController {
   @ApiResponse({ type: ResponseVideoListPageDto })
   @Get('/video/:keyword')
   영화검색리스트() {}
+
+  @ApiOperation({
+    summary: '영화 키워드 추천',
+    description:
+      '넷플릭스, 또는 영화진흥위원회 등의 인기순위를 이용해 유저에게 영화 관련 키워드를 추천',
+  })
+  @ApiResponse({ type: ResponseKeywordDto })
+  영화키워드추천() {}
 }
 
 @Controller('comment')
